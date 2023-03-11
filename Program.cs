950159a816f295d4fb0bfe86bc21c33b5a02738b
gregnani <fernandou.gregnani@gmail.com>
2023-03-10 22:10:37 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("cidades");
+Console.WriteLine("Santos");
+Console.WriteLine("São vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Guaruja");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Cananeia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha comprida");
